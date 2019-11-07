ff3325d07e16b0851c1520cab159c8984a32e885
RyanKendrick <rpkendrick@gmail.com>
2019-11-07 14:32:07 -0500

    defined routes for login, admin registration and user registration

--- server/app.js
+++ server/app.js
@@ -41,3 +41,33 @@ router.post('/register', function(req, res) {
     });
   });
 });
+
+//Route definition for registering an admin
+router.post('/register-admin', function(req, res) {
+  db.insertAdmin([
+    req.body.name,
+    req.body.email,
+    bcrypt.hashsync(req.body.password, 8),
+    1
+  ],
+  function (err) {
+    if (err) return res.status(500).send("There was a problem regestering Admin.")
+    db.selectByEmail(req.body.email, (err, user) => {
+      if (err) return res.status(500).send("There was a problem getting the Admin.")
+      let token = jwt.sign({ id: user.id }, config.secret, { exiresIn: 86400 });
+      res.status(200).send({ auth: true, token: token, user: user});
+    });
+  });
+});
+
+//Route definition for login
+router.post('/login', (req, res) => {
+  db.selectByEmail(req.body.email, (err, user) => {
+    if (err) return res.status(500).send("Error on the server.");
+    if (!user) return res.status(404).send("No user found");
+    let passwordIsValid = bcrypt.compareSync(req.body.password, user.user_pass);
+    if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+    let token = jwt.sign({ id: user.id }, config.secret, { expiresIn: 86400 });
+    res.status(200).send({ auth: true, token: token, user: user });
+  });
+});
