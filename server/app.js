48f5ab59b8c73a047f89b8890ef7d0c2b2ab0a8c
RyanKendrick <rpkendrick@gmail.com>
2019-11-07 14:06:17 -0500

    defined route for user registration

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Server setup
 const express = require('express');
 const DB = require('./db');
 const config = require('./config');
@@ -13,3 +14,30 @@ const router = express.Router();
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
+
+// CORS middleware
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+}
+
+app.use(allowCrossDomain);
+
+
+//Route definition for registering new user
+router.post('/register', function(req, res) {
+  db.insert([
+    req.body.name,
+    req.body.email,
+    bcrypt.hashSync(req.body.password, 8)
+  ],
+  function (err) {
+    if (err) return res.status(500).send("There was a problem registering the user.")
+    db.selectByEmail(req.body.email, (err,user) => {
+      if (err) return res.status(500).send("There was a problem getting user.")
+      let token - jwt.sign({ id: user.id }, config.secret, {expiresIn: 86400});
+      res.status(200).send({ auth: true, token: token, user: user});
+    });
+  });
+});
