1ea565264d0229d62e33e485cbc15b908ae98b81
RyanKendrick <rpkendrick@gmail.com>
2019-11-13 14:00:02 -0500

    added script for handling login submission in Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,3 +1,4 @@
+<!-- Login page markup -->
 <template>
   <div>
     <h4>Login</h4>
@@ -20,3 +21,33 @@
     </form>
   </div>
 </template>
+
+<!-- Script that handles the login submission -->
+<!-- the email and password data attributes bound to the form fields to collect user input. We made a request to the server to authenticate the credentials the user supplies. -->
+<script>
+  export default {
+    data() {
+      return {
+        email: "",
+        password: ""
+      }
+    },
+    methods: {
+      handleSubmit(e) {
+        e.preventDefault()
+        if (this.password.length > 0) {
+          this.$http.post('http://localhost:3000/login', {
+            email: this.email,
+            password: this.password
+          })
+          .then(response => {
+
+          })
+          .catch(function (error) {
+            console.error(error.response);
+          });
+        }
+      }
+    }
+  }
+</script>
