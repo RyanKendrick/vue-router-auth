50f166e64689d8e8d01c62903a7b2d272547e101
RyanKendrick <rpkendrick@gmail.com>
2019-11-13 15:30:10 -0500

    built template markup for Register.vue component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -22,8 +22,7 @@
   </div>
 </template>
 
-<!-- Script that handles the login submission -->
-<!-- the email and password data attributes bound to the form fields to collect user input. We made a request to the server to authenticate the credentials the user supplies. -->
+<!-- Script that handles the login submission - the email and password data attributes bound to the form fields to collect user input. We made a request to the server to authenticate the credentials the user supplies. -->
 <script>
   export default {
     data() {
@@ -40,8 +39,25 @@
             email: this.email,
             password: this.password
           })
+          // server response: We store the jwt token and user information in localStorage so we can access it from all parts of our application. Of course, we redirect the user to whichever part of our application they tried to access before being redirected to login
           .then(response => {
+            let is_admin = response.data.user.is_admin
+            localStorage.setItem('user', JSON.stringify(response.data.user))
+            localStorage.setItem('jwt', response.data.token)
 
+            if (localStorage.getItem('jwt') != null) {
+              this.$emit('loggedIn')
+              if (this.$route.params.nextUrl != null) {
+                this.$router.push(this.$route.params.nextUrl)
+              }
+              else {
+                if (is_admin == 1) {
+                  this.$router.push('admin')
+                } else {
+                  this.$router.push('dashboard')
+                }
+              }
+            }
           })
           .catch(function (error) {
             console.error(error.response);
